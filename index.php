92d9e40f86786a827b1e3063f30316d68e6ed295
hamisi <kilumohamisi@gmail.com>
2023-02-03 17:24:21 +0300

    added function

--- index.php
+++ index.php
@@ -18,10 +18,7 @@
         component();
         component();
         component();
-        component();
-        component();
-        component();
-        component();
+       
       ?>
       
     </div>
