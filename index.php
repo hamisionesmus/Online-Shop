1381d63e5bf19ff72ec51e56fffe890fa66c8e35
hamisi <kilumohamisi@gmail.com>
2023-02-03 03:23:29 +0300

    onlineshop

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./CSS/style.css" />
+    <title>Online Shopping</title>
+  </head>
+  <body>
+    <!-- container -->
+    <div class="container">
+      <!-- product-1 -->
+      <div class="product">
+        <div class="product-image">
+          <img src="./IMAGES/laptop.jpg" alt="product" />
+        </div>
+        <div class="product-details">
+        <p class="product-name">Product 1</p>
+          <p class="product-description">
+            laptop
+          </p>
+        
+        <div class="product-price">
+          <span>$100</span>
+          <button class="btn">Add to Cart</button>
+        </div>
+        </div>
+      </div>
+      <!-- product-2 -->  
+      <div class="product">
+        <div class="product-image">
+          <img src="./IMAGES/laptop.jpg" alt="product" />
+        </div>
+        <div class="product-details">
+        <p class="product-name">Product 1</p>
+          <p class="product-description">
+            laptop
+          </p>
+        
+        <div class="product-price">
+          <span>$100</span>
+          <button class="btn">Add to Cart</button>
+        </div>
+        </div>
+      </div>
+        <!-- product-3 -->
+      <div class="product">
+        <div class="product-image">
+          <img src="./IMAGES/laptop.jpg" alt="product" />
+        </div>
+        <div class="product-details">
+        <p class="product-name">Product 1</p>
+          <p class="product-description">
+            laptop
+          </p>
+        
+        <div class="product-price">
+          <span>$100</span>
+          <button class="btn">Add to Cart</button>
+        </div>
+        </div>
+      </div>
+      <!-- product-4 -->
+      <div class="product">
+        <div class="product-image">
+          <img src="./IMAGES/laptop.jpg" alt="product" />
+        </div>
+        <div class="product-details">
+          <p class="product-name">product name</p>
+          <p class="product-description">
+            laptop
+          </p>
+        
+        <div class="product-price">
+          <span>$100</span>
+          <button class="btn">Add to Cart</button>
+        </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
