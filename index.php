eb2c0d2f4c3f3064757a2504b46ea8263abdff70
hamisi <kilumohamisi@gmail.com>
2023-02-03 20:28:08 +0300

    added header

--- index.php
+++ index.php
@@ -17,8 +17,11 @@
       <div class="header">
         <h3>Shop</h3>
         <div class="search">
-          <input type="text" placeholder="Search an item" />
-          <button classs="btn-search">Search</button>
+          <input type="text" placeholder="search product" />
+          <button classs="btn-search"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+</svg>
+</button>
         </div>
         <h3>Bucket</h3>
       </div>
